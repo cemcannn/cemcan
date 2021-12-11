7a78bd67d887b33ca3631cadd30c174800b95c6f
cemcannn <cmcan@windowslive.com>
2021-12-12 00:35:01 +0300

    commit

--- 15-Lesson_11-12.12.2021/1-requests.py
+++ 15-Lesson_11-12.12.2021/1-requests.py
@@ -12,19 +12,24 @@
 # https://httpbin.org
 
 
-
 import requests
 
-r = requests.get("https://httpbin.org/get")
+r = requests.get("https://httpbin.org") # requests modülünün get fonksiyonu ile çekilir ve "r" değişkenine eşitlenir.
 r2 = requests.get("https://httpbin.org/post", data={"isim":"murat cabuk"})
 
-print(r)
+print(r) # "r" değişkeni yazdırılınca cevap (Response) olarak bize "200" rakamını verir bu rakamların bir anlamı var.
 
-# requestin soınucunu ekrana yazdırıyoruz
-print(r.text)
+# # Bu kodların anlamları
+# Informational responses (100–199) -> 100-199 arası
+# Successful responses (200–299) -> 200-299 arası
+# Redirection messages (300–399) -> 300-399 arası
+# Client error responses (400–499) -> 400-499 arası
+# Server error responses (500–599) -> 500-599 arası
 
+# # requestin sonucunu ekrana yazdırıyoruz yani bütün html kodunu yazdırır ekrana.
+# print(r.text)
 
-print(r2.text)
+print(r2.text) # Sitenin HTML içeriğini döndürür.
 
 
 
@@ -32,7 +37,7 @@ print(r2.text)
 
 response_github = requests.get("https://api.github.com/events")
 
-status_code = response_github.status_code
+status_code = response_github.status_code # İsteğin HTTP durum kodunu döndürür.
 print(response_github)
 
 print(dir(response_github))
@@ -40,17 +45,17 @@ print(dir(response_github))
 print(status_code)
 
 if status_code == 200:
-    #print(response_github.json())
+    # print(response_github.json())
 
     print(type(response_github.json()))
 
     print(len(response_github.json()))
-    #print(response_github.json()[10])
+    print(response_github.json()[10])
     print(type(response_github.json()[10]))
     print("-------------------------------------------")
     print(response_github.json()[10]["actor"]["url"])
     print("-------------------------------------------")
-    #print(response_github.text)
+    # print(response_github.text)
     print(type(response_github.text))
 
     print("------------------------------")
@@ -68,8 +73,8 @@ if status_code == 200:
 print("------------------------------------------------------------redirect history")
 request_maps_google = requests.get("https://maps.google.com")
 print(request_maps_google.status_code)
-print(request_maps_google.history)
-print(request_maps_google.history[0])
+print(request_maps_google.history) # Bir istek attınız ve status_code değeri 200 geldi. Ancak belki 2 kere 301 sonra 1 kere 302 yönlendirme ile en son bir sayfaya geldi ve 200 döndü. Buna bakabilmek için bu method kullanılıyor.
+print(request_maps_google.history[0]) # İlk istek
 
 
 
@@ -79,7 +84,7 @@ print(request_github.history)
 
 
 
-#####################################33 post işlemi
+# #####################################33 post işlemi
 
 r = requests.post('https://httpbin.org/post', data={'isim': 'murat çabuk', 'yil':'2021'})
 print(r.text)
@@ -90,6 +95,7 @@ print(r.text)
 r_haberler = requests.get("https://www.haberler.com/ara/", params = {"q":"Türkiye"})
 print(r_haberler.text)
 
-requests.
+# requests.
 
-
+print(r_haberler.request) #  Yaptığınız isteğin ne olduğunu döner, GET,POST v.s.
+print(r_haberler.request.method)
