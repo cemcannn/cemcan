7adc565d0d5b4a11f59526cf3d230aa4e052022b
cemcannn <cmcan@windowslive.com>
2021-12-16 12:00:15 +0300

    requests modified

--- 15-Lesson_11-12.12.2021/1-requests.py
+++ 15-Lesson_11-12.12.2021/1-requests.py
@@ -45,18 +45,18 @@ print(dir(response_github))
 print(status_code)
 
 if status_code == 200:
-    # print(response_github.json())
+    # print(response_github.json()) # print(response_github.text) ile aynı görevi görüyor fakat type liste olarak 
 
     print(type(response_github.json()))
 
-    print(len(response_github.json()))
+    print(len(response_github.json())) 
     print(response_github.json()[10])
     print(type(response_github.json()[10]))
     print("-------------------------------------------")
     print(response_github.json()[10]["actor"]["url"])
     print("-------------------------------------------")
     # print(response_github.text)
-    print(type(response_github.text))
+    print(type(response_github.text)) # string type
 
     print("------------------------------")
     print(type(response_github.cookies))
@@ -68,34 +68,34 @@ if status_code == 200:
     print(r.cookies.keys())
     print(r.cookies["logged_in"])
     
+######################################## REDIRECTION VE HISTORY
 
-##############################################3 redirection ve history
 print("------------------------------------------------------------redirect history")
 request_maps_google = requests.get("https://maps.google.com")
 print(request_maps_google.status_code)
 print(request_maps_google.history) # Bir istek attınız ve status_code değeri 200 geldi. Ancak belki 2 kere 301 sonra 1 kere 302 yönlendirme ile en son bir sayfaya geldi ve 200 döndü. Buna bakabilmek için bu method kullanılıyor.
 print(request_maps_google.history[0]) # İlk istek
 
-
-
 request_github = requests.get("http://github.com/")
 print(request_github.status_code)
 print(request_github.history)
 
+######################################## PARAMETRE GÖNDERME
 
+r = requests.get("http://httpbin.org/redirect/1",allow_redirects=False) # Sayfa yönlendirmesini iptal ediyor.
+print(r.status_code) # Status kod yönlenen sayfa ise "302" verir.
 
-# #####################################33 post işlemi
+r = requests.get("http://httpbin.org/redirect/1",allow_redirects=True) # Sayfa yönlendirmesini izin veriyor.
+print(r.status_code) # Status kod yönlenen sayfa ise "200" verir.
 
-r = requests.post('https://httpbin.org/post', data={'isim': 'murat çabuk', 'yil':'2021'})
-print(r.text)
-
-
-
-
-r_haberler = requests.get("https://www.haberler.com/ara/", params = {"q":"Türkiye"})
+r_haberler = requests.get("https://www.haberler.com/ara/", params = {"q":"Türkiye"}) # Sayfa sonuna Türkiye parametresini ekler.
 print(r_haberler.text)
 
-# requests.
+r = requests.post("http://httpbin.org/post", data={"username":"cem","password":"asd123"}) # POST için sanki bir HTML formu doldurmuş ve o bilgileri post etmişiz gibi davranalım.
+print(r.json())
+print(r.json()["form"]) # Çıktıyı 'username': 'cem', 'password': 'asd123' şeklinde verir.
+
+######################################## REQUESTS
 
 print(r_haberler.request) #  Yaptığınız isteğin ne olduğunu döner, GET,POST v.s.
 print(r_haberler.request.method)
