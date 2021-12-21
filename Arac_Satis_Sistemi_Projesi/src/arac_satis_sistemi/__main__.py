0f365065f335f1fd0124d36324406569e8c17828
Cem Can <91818127+cemcannn@users.noreply.github.com>
2021-12-21 17:14:55 +0300

    Delete Arac_Satis_Sistemi_Projesi directory

--- Arac_Satis_Sistemi_Projesi/src/arac_satis_sistemi/__main__.py
+++
@@ -1,6 +0,0 @@
-import menu_yonetimi # Ana dosya menu yönetimi dosyasını çekiyor
-
-if __name__ == "__main__": # Eğer ana dosya ise çalışıyor, modül ise çalışmıyor.
-    menu_yonetimi.ana_menu_getir()
-else:
-    print("bu uygulama module olarak kullanılamaz")
