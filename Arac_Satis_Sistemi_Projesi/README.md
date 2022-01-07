e870d7d5d4580578b6d3c9fe2060415c2ef9a58e
Cem Can <91818127+cemcannn@users.noreply.github.com>
2022-01-07 19:22:35 +0300

    Delete Arac_Satis_Sistemi_Projesi directory
    This project is moved new repo.

--- Arac_Satis_Sistemi_Projesi/README.md
+++
@@ -1,6 +0,0 @@
-Araç Yönetim Sistemi Project
-----------------------------
-
-This project is created within the scope of the Bilge Adam academy.
-
-
--- Arac_Satis_Sistemi_Projesi/src/arac_satis_sistemi/__main__.py
+++
@@ -1,6 +0,0 @@
-import menu_yonetimi 
-
-if __name__ == "__main__": 
-    menu_yonetimi.ana_menu_getir()
-else:
-    print("Bu Uygulama Module Olarak Kullanılamaz.")
